146700abd30b042e1669d7975cd254f95e419096
tnsbro <flower58570@gmail.com>
2025-04-19 01:01:27 +0900

    UPATE

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,4 +1,13 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
 function NotFoundPage() {
+
+  const navigete = useNavigate();
+
+  useEffect(() => {
+    navigete('/');
+  }, []); // 컴포넌트가 마운트될 때 홈으로 리디렉션
   return (
     <div className="not-found-page">
       <h1>404 Not Found</h1>
